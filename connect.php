87d6616ddf970d14cd9412753805351fee33d51e
mungaiHub <amosmungai085@gmail.com>
2025-11-02 18:02:33 +0300

    learn how to connect using php

--- connect.php
+++ connect.php
@@ -1,23 +1,15 @@
 <?php
-	$firstName = $_POST['firstName'];
-	$lastName = $_POST['lastName'];
-	$gender = $_POST['gender'];
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$number = $_POST['number'];
+$dbserver = "localhost";
+$dbuser = "root";
+$dppassword = "";
+$dbname = "school";
+$conn = "";
 
-	// Database connection
-	$conn = new mysqli('localhost','root','','test');
-	if($conn->connect_error){
-		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
-		$execval = $stmt->execute();
-		echo $execval;
-		echo "Registration successfully...";
-		$stmt->close();
-		$conn->close();
-	}
+$conn = mysqli_connect($dbserver ,$dbuser,$dppassword,$dbname );
+if($conn){
+    echo "you are connected";
+}
+else{
+    echo "could not connect";
+}
 ?>
